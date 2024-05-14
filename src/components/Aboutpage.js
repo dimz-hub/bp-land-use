7d3b1bc407c9c3e283d2ba33f1b2b40503d6b3c3
heedrhiss <muyideenidris77@gmail.com>
2024-05-14 11:45:45 +0100

    about us

---
+++ src/components/Aboutpage.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Aboutpage() {
+    return (
+        <div className="max-w-screen mt-14 p-12 bg-gray-100 mx-auto text-center" id="about">
+            <h1 className="text-4xl font-bold my-10 ">About Tobela Real Estate Services</h1>
+            <p className="text-left text-xl">Welcome to Tobela Real Estate Services, your trusted partner in real estate investment and development. With a commitment to excellence and a track record of success, we are proud to be a government-certified real estate company specializing in obtaining Lagos State certified Planning Permits For all residential, commercial, industrial and Agricultural land.
+<br/>
+<br/>
+At Tobela Real Estate Services, we understand the importance of trust and transparency in the real estate industry. That's why we have obtained rigorous government certifications that validate our expertise, ethical practices, and compliance with industry standards. These certifications serve as a testament to our dedication to delivering exceptional service and value to our clients.
+<br/>
+<br/>
+Our team comprises experienced professionals who are passionate about real estate and dedicated to helping our clients achieve their goals. Whether you are looking to buy, sell, rent, or invest in properties, we provide personalized solutions tailored to meet your unique needs and preferences.
+<br/>
+<br/>
+What sets us apart is our commitment to integrity, professionalism, and customer satisfaction. We strive to build long-term relationships with our clients based on mutual trust, respect, and open communication. When you partner with us, you can rest assured that you are in capable hands backed by industry expertise and government recognition.
+<br/>
+<br/>
+Explore our range of services and discover why Tobela Real Estate Services is the preferred choice for discerning individuals and investors in the real estate market. Contact us today to experience the difference firsthand and unlock the potential of your real estate journey.</p>
+        </div>
+    )
+}
+
+export default Aboutpage
--- src/components/Toppage.jsx
+++ src/components/Toppage.jsx
@@ -6,8 +6,8 @@ export default function Toppage() {
         <div className='w-[80%] m-[auto]  '>
             <div className='flex flex-col justify-center items-center w-[100%] h-[80vh]' >
  
-              <h1 className='text-[white] text-[50px]'>
-              LAND USE CHARGE
+              <h1 className='text-[white] text-[50px] font-bold'>
+              Lagos State Develpers Planning Permits
               </h1>
               <p className='text-[white] text-[35px]'>
 
