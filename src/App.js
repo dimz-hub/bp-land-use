2835526bd13ec1a03487f14b4c2c1a35efca04f6
olawale oladimeji <oladimejiolawale1999@gmail.com>
2024-05-15 11:44:34 +0100

    form done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {Route, Routes} from 'react-router-dom'
 import Landingpage from "./components/Landingpage";
+import DocumentForm from "./components/DocumentForm";
+
+
 function App() {
   return (
     <div >
      <Routes>
       <Route path="/" element = {<Landingpage />}/>
+      <Route path="/obtain-document" element = {<DocumentForm />}/>
       </Routes>
     </div>
   );
